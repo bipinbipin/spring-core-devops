37798c59545f2a6445a01f17386285e2af67726d
Bipin Butala <bipinbipin@gmail.com>
2016-10-14 14:54:31 -0500

    Breaking the build.  CI test

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringCoreDevOpsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
+		//SpringApplication.run(SpringCoreDevOpsApplication.class, args);
 
         //This is a whitespace test
         // here i am testing the webhook
